3b4137f2031e72cfbbe7e258dc3ecd47d3e901e2
Jérémie Astor <astor.jeremie@wanadoo.fr>
2020-02-20 21:05:39 +0100

    Update summary.sh

--- summary.sh
+++ summary.sh
@@ -3,5 +3,5 @@ cd html
 for dir in */
 do
   branch=$(basename $dir)
-  echo "  * [$file](https://fennecdjay.github.com/gwion-coverage-report/$branch)
+  echo "  * [$file](https://fennecdjay.github.com/gwion-coverage-report/$branch)"
   done > README.md
