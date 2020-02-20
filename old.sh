50429dc6c99ee3d91626609afcb5554524577cdb
Jérémie Astor <astor.jeremie@wanadoo.fr>
2020-02-20 20:52:49 +0100

    Update old.sh

--- old.sh
+++ old.sh
@@ -1,8 +1,8 @@
 # $1 : branch
 
-if [ -d gwion-coverage-report/html/$1 ]
+if [ -d html/$1 ]
 then
-    for report in gwion-coverage-report/html/$1/*.txt
+    for report in html/$1/*.txt
     do cp $report $(basename $report).old
     done
 fi  
