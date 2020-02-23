d64d8fb332c686f264b0fc012f4bd299f30e93b4
GitHub Action <action@github.com>
2020-02-23 13:43:01 +0000

    Update html report

--- old.sh
+++
@@ -1,9 +0,0 @@
-# $1 : branch
-
-if [ -d html/$1 ]
-then
-    for report in html/$1/*.txt
-    do cp $report $(basename $report).old
-    done
-else mkdir -p html/$1
-fi  
--- summary.sh
+++
@@ -1,7 +0,0 @@
-cd html
-
-for dir in */
-do
-  branch=$(basename $dir)
-  echo "  * [$branch](https://fennecdjay.github.io/gwion-coverage-report/$branch) ([diff](https://fennecdjay.github.io/gwion-coverage-report/$branch/diff.html))"
-  done > README.md
