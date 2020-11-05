6e3251f0a6cb0c102bb7454e4504791f961d7559
Jérémie Astor <fennecdjay@gmail.com>
2020-11-05 16:42:48 +0100

    Update summary.sh

--- summary.sh
+++ summary.sh
@@ -3,5 +3,5 @@ cd html
 for dir in */
 do
   branch=$(basename $dir)
-  echo "  * [$branch](https://fennecdjay.github.io/gwion-coverage-report/$branch) ([diff](https://fennecdjay.github.io/gwion-coverage-report/$branch/diff.html))"
+  echo "  * [$branch](https://Gwion.github.io/gwion-coverage-report/$branch) ([diff](https://Gwion.github.io/gwion-coverage-report/$branch/diff.html))"
   done > README.md
